b84213e4d440aa03a985e99908fd9ee16c755a51
ittekikun <ittekikun.g@gmail.com>
2016-05-29 20:20:36 +0900

    とりあえずコミット奴

---
+++ src/main/java/com/ittekikun/plugin/itkcore/ITKCore.java
@@ -0,0 +1,18 @@
+package com.ittekikun.plugin.itkcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ITKCore  extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+
+    }
+
+    @Override
+    public void onDisable()
+    {
+
+    }
+}
